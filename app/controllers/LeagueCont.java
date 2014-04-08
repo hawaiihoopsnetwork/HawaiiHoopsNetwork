66427450d84b758629604ad63db4fc34061a44b4
Andrew Pearson-Wood <andrewpw133@gmail.com>
2014-04-08 12:10:12 -1000

    Add the controller

--- app/controllers/LeagueCont.java
+++ app/controllers/LeagueCont.java
@@ -1,5 +1,23 @@
 package controllers;
 
+import java.util.List;
+import models.teams.Team;
+import models.teams.TeamDB;
+import play.mvc.Result;
+import views.html.leagues.Leagues;
+
 public class LeagueCont {
 
+  /**
+   * returns the LeaguesPage
+   * 
+   * @return the LeaguesPage
+   */
+  public static Result leagues() {
+    List<Team> listTeam = TeamDB.getTeams();
+    
+    return ok(Leagues.render("Leagues", listTeam));
+  }
+
+  
 }
